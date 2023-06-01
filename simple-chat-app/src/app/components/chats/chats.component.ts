2e9efec9f9409873561c6846bfe3989838a54096
ramedark <52680013+ramedark@users.noreply.github.com>
2023-06-01 12:21:31 +0200

    fixes

--- simple-chat-app/src/app/components/chats/chats.component.ts
+++ simple-chat-app/src/app/components/chats/chats.component.ts
@@ -20,21 +20,42 @@ export class ChatsComponent implements OnInit {
 
     this.chatService.userChanged.subscribe((id) => {
       this.id = id;
-      this.messages = this.chatService.getUserChats(this.id);
       this.selectedUser = this.chatService.getUser(this.id);
+      this.loadMessages();
     });
+
+    this.chatService.groupChanged.subscribe((id) => {
+      this.id = id;
+      this.selectedUser = this.chatService.getGroup(this.id);
+      this.loadMessages();
+    });
+
     this.chatService.messageReceived.subscribe((data: any) => {
       if (data.id == this.id) {
-        this.messages = this.chatService.getUserChats(this.id);
+        this.loadMessages();
       }
     });
   }
+
+  loadMessages() {
+    if (this.selectedUser) {
+      this.messages = this.chatService.getUserChats(this.id);
+    } else {
+      this.messages = this.chatService.getGroupChats(this.id);
+    }
+  }
+
   onSend() {
     if (this.newMessage.trim()) {
-      this.chatService.sendMessage(this.id, this.newMessage);
+      if (this.selectedUser) {
+        this.chatService.sendMessage(this.id, this.newMessage);
+      } else {
+        this.chatService.sendMessageToGroup(this.id, this.newMessage);
+      }
       this.newMessage = '';
     }
   }
+
   getFirstLetters(name: string): string {
     const nameParts = name.split(' ');
     const firstLetter = nameParts[0].charAt(0).toUpperCase();
